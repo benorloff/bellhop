57eb43f387e13589befb48ffcd979d899e89f7f7
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-11-30 16:43:43 -0700

    navbar

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,12 @@
+export const Navbar = () => {
+    return (
+        <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+            {/* TODO: Mobile Sidebar */}
+            <div className="flex items-center gap-x-4">
+                <div className="hidden md:flex">
+                    Logo
+                </div>
+            </div>
+        </nav>
+    )
+};
