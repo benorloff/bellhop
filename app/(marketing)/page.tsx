86118fb4e006dfabd87e59d700fed709a0962c0d
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-12-01 18:21:03 -0700

    import shadcn button

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -15,6 +15,9 @@ const MarketingPage = () => {
                     WordPress hosting.
                 </div>
             </div>
+            <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+                Collaborate, manage your website, and reach new productivity peaks. From high rises to the home office, the way your team works is unique — accomplish it all with Bellhop.
+            </div>
         </div>
     )
 };
