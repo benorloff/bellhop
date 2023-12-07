21a8829d9581c2d1a4d7b633762067c9f8df1608
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-12-07 14:31:59 -0700

    reduce sidebar width

--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ const DashboardLayout = ({ children }: {
             <Navbar />
             <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
                 <div className="flex gap-7">
-                    <div className="w-64 shrink-0 hidden md:block">
+                    <div className="w-48 shrink-0 hidden md:block">
                         <Sidebar />
                     </div>
                     {children}
