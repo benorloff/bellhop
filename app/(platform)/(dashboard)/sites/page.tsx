949a45cf013dfb7087527048867864cd5e5da04c
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2023-12-09 10:51:49 -0700

    sitelist skeleton styling

--- app/(platform)/(dashboard)/sites/page.tsx
+++ app/(platform)/(dashboard)/sites/page.tsx
@@ -3,10 +3,12 @@ import { SiteList } from "./_components/site-list";
 
 const SitesPage = () => {
     return ( 
-        <div>
-            <Suspense fallback={<SiteList.Skeleton />}>
-                <SiteList />
-            </Suspense>
+        <div className="w-full mb-20">
+            <div className="px-2 md:px-4">
+                <Suspense fallback={<SiteList.Skeleton />}>
+                    <SiteList />
+                </Suspense>
+            </div>
         </div>
      );
 }
