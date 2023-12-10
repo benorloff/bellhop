5191ebe8197ea21b86ae41121f44b4ce9663a921
Maya Ellington <ellingtonmaya@gmail.com>
2023-12-10 01:59:54 -0800

    wip hide secret

--- app/(platform)/(dashboard)/_components/ticket-list.tsx
+++ app/(platform)/(dashboard)/_components/ticket-list.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Ticket } from '../../../../types/index'
+import Link from 'next/link';
+import { Ticket } from '@/types/index'
+
+const apiKey = process.env.FRESHDESK_KEY;
+const freshdeskApiUrl = 'https://bellhop.freshdesk.com/api/v2/tickets';
 
 
 
@@ -10,16 +14,14 @@ const TicketList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const freshdeskApiUrl = 'https://bellhop.freshdesk.com/api/v2/tickets';
-        const apiKey = 's0poIM6dZpKt4nyhVcDf';
-
+        console.log(apiKey)
         const response = await axios.get(freshdeskApiUrl, {
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Basic ${btoa(`${apiKey}:x`)}`, // Using basic authentication
           },
         });
-
+console.log(apiKey)
         setTickets(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -43,7 +45,11 @@ const TicketList = () => {
           {tickets.map((ticket: Ticket) => (
             <tr key={ticket.id}>
               <td className="border p-2">{ticket.status}</td>
-              <td className="border p-2">{ticket.subject}</td>
+              <td className="border p-2"> 
+                {/* Use Link component for navigation to the single ticket page */}
+                <Link href={`/tickets/${ticket.id}`}>
+                  {ticket.subject}
+                </Link></td>
               <td className="border p-2">{new Date(ticket.updated_at).toLocaleString()}</td>
             </tr>
           ))}
--- app/(platform)/(dashboard)/tickets/page.tsx
+++ app/(platform)/(dashboard)/tickets/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 import TicketList from "../_components/ticket-list";
+import { Suspense } from "react";
 
 const TicketsPage = () => {
 
     return ( 
         <div>
             <div id="title" className="text-3xl">Support tickets</div>
+            <Suspense fallback={<TicketList.Skeleton />}>
             <TicketList />
+            </Suspense>
         </div>
      );
 }
