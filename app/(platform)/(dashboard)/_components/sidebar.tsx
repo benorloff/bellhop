8abc3b6ab197a6ca6310b2ad79cfbd0546ee2644
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-01-18 20:15:39 -0700

    adjust dashboard layout width to accommodate new sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -14,7 +14,7 @@ export const Sidebar = ({
     storageKey,
 }: SidebarProps) => {
     return (
-        <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+        <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-48 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
             <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
                 <ul className="space-y-2 font-medium">
                     {/* These list items should be mapped abstracted to a NavItem component */}
