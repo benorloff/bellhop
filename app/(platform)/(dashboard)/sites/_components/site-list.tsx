b5834ffc3bdbe782d55258b0c93b01f6ff51c5ef
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-01-29 19:10:49 -0700

    update site list responsiveness

--- app/(platform)/(dashboard)/sites/_components/site-list.tsx
+++ app/(platform)/(dashboard)/sites/_components/site-list.tsx
@@ -22,7 +22,7 @@ export const SiteList = async () => {
 
     return (
         <div className="space-y-4">
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {sites.map((site) => (
                     <Link
                         key={site.id}
