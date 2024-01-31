71398375a89a4720b8a66b8567240ab8c83b02e5
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-01-30 17:02:55 -0700

    create-ticket types

---
+++ actions/create-ticket/types.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+import { ActionState } from "@/lib/create-safe-action";
+
+import { CreateTicket } from "./schema";
+
+export type InputType = z.infer<typeof CreateTicket>;
+export type ReturnType = ActionState<InputType, InputType>;
