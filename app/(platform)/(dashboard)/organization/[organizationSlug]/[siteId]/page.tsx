1ec5416ab26a29c94cc16a3480f69c6b36126e6d
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-02-01 18:41:56 -0700

    refactor site page UI

--- app/(platform)/(dashboard)/organization/[organizationSlug]/[siteId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationSlug]/[siteId]/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 interface SiteIdPageProps {
     params: {
@@ -57,28 +57,44 @@ const SiteIdPage = async ({
                     <Card>
                         <CardHeader>
                             <CardTitle>Tickets</CardTitle>
-                            <CardContent>list goes here</CardContent>
                         </CardHeader>
+                        <CardContent>
+                            {tickets.results.map((ticket: any) => (
+                                <div key={ticket.id} className="flex flex-row gap-4 justify-between rounded-lg bg-[#FFFFFF] py-2">
+                                    <Badge>Status {ticket.status}</Badge>
+                                    <div className="grow">{ticket.subject}</div>
+                                    <div className="shrink">{new Date(ticket.updated_at).toLocaleString()}</div>
+                                </div>
+                            ))}
+                        </CardContent>
+                        <CardFooter>
+                            <Button>View all tickets</Button>
+                        </CardFooter>
                     </Card>
-                    <div className="bg-white rounded-sm p-4 shadow">
-                        <div className="text-xl mb-4">Tickets</div>
-                        {/* Ticket List component goes here. */}
-                        {tickets.results.map((ticket: any) => (
-                            <div key={ticket.id} className="flex flex-row gap-4 justify-between rounded-lg bg-[#FFFFFF] py-2">
-                                <Badge>Status {ticket.status}</Badge>
-                                <div className="grow">{ticket.subject}</div>
-                                <div className="shrink">{new Date(ticket.updated_at).toLocaleString()}</div>
-                            </div>
-                        ))}
-                    </div>
                 </div>
                 <div className="flex flex-col grow basis-auto gap-8">
-                    <div className="bg-white rounded-sm p-4 shadow">
-                        <div className="text-xl mb-4">Team</div>
-                    </div>
-                    <div className="bg-white rounded-sm p-4 shadow">
-                        <div className="text-xl mb-4">IP Address</div>
-                    </div>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Team</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            Team member list goes here.
+                        </CardContent>
+                        <CardFooter>
+                            <Button>Invite Member</Button>
+                        </CardFooter>
+                    </Card>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>IP Address</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            IP address goes here.
+                        </CardContent>
+                        {/* <CardFooter>
+                            <Button>Invite Member</Button>
+                        </CardFooter> */}
+                    </Card>
                 </div>
             </div>
         </div>
