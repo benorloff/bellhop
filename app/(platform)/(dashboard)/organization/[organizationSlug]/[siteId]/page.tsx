5daaedc567ee8a3f796fef58e4460191ea468e4d
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-02-01 18:31:41 -0700

    shadcn card and badge

--- app/(platform)/(dashboard)/organization/[organizationSlug]/[siteId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationSlug]/[siteId]/page.tsx
@@ -2,6 +2,9 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 interface SiteIdPageProps {
     params: {
@@ -51,12 +54,18 @@ const SiteIdPage = async ({
             </div>
             <div className="flex flex-row flex-wrap gap-8">
                 <div className="flex flex-col basis-full lg:basis-7/12 gap-8">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Tickets</CardTitle>
+                            <CardContent>list goes here</CardContent>
+                        </CardHeader>
+                    </Card>
                     <div className="bg-white rounded-sm p-4 shadow">
                         <div className="text-xl mb-4">Tickets</div>
                         {/* Ticket List component goes here. */}
                         {tickets.results.map((ticket: any) => (
                             <div key={ticket.id} className="flex flex-row gap-4 justify-between rounded-lg bg-[#FFFFFF] py-2">
-                                <div className="shrink">{ticket.status}</div>
+                                <Badge>Status {ticket.status}</Badge>
                                 <div className="grow">{ticket.subject}</div>
                                 <div className="shrink">{new Date(ticket.updated_at).toLocaleString()}</div>
                             </div>
