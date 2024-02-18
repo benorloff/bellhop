86312592360f3329663232613c291950fbe3cee5
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-02-18 13:55:50 -0700

    upload dropzone for image upload modal

--- components/modals/site-image-modal.tsx
+++ components/modals/site-image-modal.tsx
@@ -1,18 +1,55 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+
 import { useModal } from "@/hooks/use-modal-store";
+
 import { 
     Dialog,
     DialogContent,
     DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle
 } from "@/components/ui/dialog";
+import { 
+    Form, 
+    FormControl, 
+    FormField, 
+    FormItem, 
+    FormLabel, 
+    FormMessage 
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "../ui/input";
+import { FileUpload } from "../file-upload";
+import { useState } from "react";
+
+const formSchema = z.object({
+    imageUrl: z.string(),
+});
+
 
 export const SiteImageModal = () => {
     const { onOpen, isOpen, onClose, type, data } = useModal();
 
+    const [fileUrl, setFileUrl] = useState("");
+
     const isModalOpen = isOpen && type === "siteImage";
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            imageUrl: "",
+        }
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
 
     return (
         <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -25,11 +62,42 @@ export const SiteImageModal = () => {
                         Add an image to your site.
                     </DialogDescription>
                 </DialogHeader>
-                <div>
-                    Upload dropzone will go here.
-                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="imageUrl"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Subject
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="Subject"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FileUpload 
+                            onChange={fileUrl => setFileUrl(fileUrl!)}
+                            value={fileUrl}
+                            endpoint="siteImage"
+                        />
+                        <DialogFooter>
+                            <Button 
+                                variant="primary"
+                                className="w-full"
+                            >
+                                Submit
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
             </DialogContent>
-
         </Dialog>
     )
 };
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,7 +3,8 @@
 import { useEffect, useState } from "react";
 
 import { InviteModal } from "@/components/modals/invite-modal";
-import { CreateTicketModal } from "../modals/create-ticket-modal";
+import { CreateTicketModal } from "@/components/modals/create-ticket-modal";
+import { SiteImageModal } from "@/components/modals/site-image-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -20,6 +21,7 @@ export const ModalProvider = () => {
         <>
             <InviteModal />
             <CreateTicketModal />
+            <SiteImageModal />
         </>
     )
 }
