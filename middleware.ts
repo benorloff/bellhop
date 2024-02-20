759b530b536a8a8040af481407ffdcd8c2904bd2
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-02-19 18:31:32 -0700

    update public routes for webhook

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/api/uploadthing(.*)", "/api/webhooks(*)"],
+  publicRoutes: ["/", "/api/uploadthing(.*)", "/api/webhooks/clerk(.*)"],
   // afterAuth(auth, req) {
   //   // Add auth middleware here
   //   if (!auth.userId && !auth.isPublicRoute) {
