bc4cfdf9b808ab7c6edf1be1ce669ef434d18f58
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-02-20 16:19:52 -0700

    ticket reply panel positioning

--- app/(platform)/(dashboard)/sites/page.tsx
+++ app/(platform)/(dashboard)/sites/page.tsx
@@ -3,7 +3,7 @@ import { SiteList } from "../../../../components/sites/site-list";
 
 const SitesPage = () => {
     return ( 
-        <div className="w-full mb-20">
+        <div className="w-full">
             <div className="text-3xl mb-8">Sites</div>
             <Suspense fallback={<SiteList.Skeleton />}>
                 <SiteList />
