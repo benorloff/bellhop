d9d4c1bcdaa5e3277e6fb550cf68267e593cde09
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-02-27 10:42:49 -0700

    dashboard placeholder content

--- app/(platform)/(dashboard)/dashboard/page.tsx
+++ app/(platform)/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { currentGreeting } from "@/lib/current-greeting";
 
 const DashboardPage = async () => {
@@ -5,8 +6,22 @@ const DashboardPage = async () => {
     const greeting = await currentGreeting();
 
     return ( 
-        <div>
+        <div className="flex flex-col gap-4">
             <div className="text-3xl mb-8">{greeting}</div>
+            <div className="flex flex-row gap-4">
+                <Card className="grow">
+                    <CardHeader>Site</CardHeader>
+                    <CardContent>Content goes here.</CardContent>
+                </Card>
+                <Card className="grow">
+                    <CardHeader>Site</CardHeader>
+                    <CardContent>Content goes here.</CardContent>
+                </Card>
+                <Card className="grow">
+                    <CardHeader>Site</CardHeader>
+                    <CardContent>Content goes here.</CardContent>
+                </Card>
+            </div>
         </div>
      );
 }
