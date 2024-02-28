c05a411892a176bffdcb901720173358ef353a93
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-02-28 12:07:46 -0700

    site team components

--- app/(platform)/(dashboard)/organization/[organizationSlug]/[siteId]/team/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationSlug]/[siteId]/team/page.tsx
@@ -1,10 +1,14 @@
-import { Hint } from "@/components/hint";
+import { FormInput } from "@/components/form/form-input";
+import { SiteNav } from "@/components/sites/site-nav";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { db } from "@/lib/db";
-import { MenuIcon, MoreHorizontal } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { MoreHorizontal } from "lucide-react";
+import Link from "next/link";
 
 interface SiteTeamPageProps {
     params: {
@@ -32,20 +36,36 @@ const SiteTeamPage = async ({
                     <CardTitle>Invite people</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    Invite form goes here.
+                    {/* This is a placeholder for the form to invite people to the site. */}
+                    <FormInput 
+                        id="email"
+                        label="Email"
+                        type="email"
+                        placeholder="bella@bellhop.com"
+                    />
                 </CardContent>
                 <CardFooter>
                     <Button>Invite</Button>
                 </CardFooter>
             </Card>
             <Card>
-                <CardHeader>
+                <CardHeader
+                    className="border-b"
+                >
                     <CardTitle>Team Members</CardTitle>
                 </CardHeader>
-                <CardDescription>
-
+                <CardDescription
+                    className="border-b py-2 px-6"
+                >
+                    {/* TODO: Store state and connect these buttons */}
+                    <div className="flex flex-row gap-4 items-center">
+                        <Button variant="ghost">Members</Button>
+                        <Button variant="ghost">Invitations</Button>
+                    </div>
                 </CardDescription>
-                <CardContent>
+                <CardContent
+                    className="p-6"
+                >
                     <div className="flex flex-col gap-4">
                         {members.map((member) => (
                             <div key={member.id} className="flex flex-row gap-4 items-center">
