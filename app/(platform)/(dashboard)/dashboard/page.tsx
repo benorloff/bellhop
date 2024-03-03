d81c94cc09c82328c3a2897dd3824b42c83eb834
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-03-02 22:33:35 -0700

    dashboard sites

--- app/(platform)/(dashboard)/dashboard/page.tsx
+++ app/(platform)/(dashboard)/dashboard/page.tsx
@@ -1,28 +1,40 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { currentGreeting } from "@/lib/current-greeting";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 
 const DashboardPage = async () => {
 
+    const { userId, orgId } = auth();
+    const profile = await currentProfile(userId!);
     const greeting = await currentGreeting();
 
+    const sites = await db.site.findMany({
+        where: {
+            members: {
+                some: 
+                {
+                    profileId: profile?.id,
+                }
+            }
+        }
+    });
+
     return ( 
-        <div className="flex flex-col gap-4">
+        <>
             <div className="text-3xl mb-8">{greeting}</div>
-            <div className="flex flex-row gap-4">
-                <Card className="grow">
-                    <CardHeader>Site</CardHeader>
-                    <CardContent>Content goes here.</CardContent>
-                </Card>
-                <Card className="grow">
-                    <CardHeader>Site</CardHeader>
-                    <CardContent>Content goes here.</CardContent>
-                </Card>
-                <Card className="grow">
-                    <CardHeader>Site</CardHeader>
-                    <CardContent>Content goes here.</CardContent>
-                </Card>
+            <div className="flex flex-row flex-wrap gap-4">
+                {sites.map((site) => (
+                    <div key={site.id} className="min-w-[300px]">
+                        <Card>
+                            <CardHeader>{site.name}</CardHeader>
+                            <CardContent>{site.url}</CardContent>
+                        </Card>
+                    </div>
+                ))}
             </div>
-        </div>
+        </>
      );
 }
  
