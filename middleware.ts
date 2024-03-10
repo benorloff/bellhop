637802f9a77985c29034527cad01481bfab37767
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-03-10 11:50:00 -0700

    augment clerk session claims for onboarding

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,31 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
  
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/", "/pricing", "/api/uploadthing(.*)", "/api/webhooks/(.*)"],
-  afterAuth(auth, req) {
+  afterAuth: async (auth, req: NextRequest) => {
+    const { userId, orgId, sessionClaims } = auth;
     // Handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
+    if (!userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url })
     }
     // Redirect logged in users to organization selection page if they are not active in an organization
-    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+    if (userId && !orgId && req.nextUrl.pathname !== "/select-org") {
       const orgSelection = new URL("/select-org", req.url);
       return NextResponse.redirect(orgSelection);
     }
     // If the user is logged in and trying to access a protected route, allow them to access route
-    if (auth.userId && !auth.isPublicRoute) {
+    if (userId && !auth.isPublicRoute) {
       return NextResponse.next();
     }
     // Allow users visiting public routes to access them
-    return NextResponse.next();
+    if (auth.isPublicRoute) return NextResponse.next();
   },
-  // debug: true
+  // Change debug to true to see more detailed auth logs
+  debug: false,
 });
  
 export const config = {
