52e79d6459d019f67752314ef0e25766734c3785
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2024-04-01 13:42:26 -0700

    fix feature carousel scrollprogress bug

--- components/marketing/feature-carousel.tsx
+++ components/marketing/feature-carousel.tsx
@@ -1,109 +1,41 @@
 "use client"
 
 import { cn } from "@/lib/utils"
-import Image from "next/image"
-import React, { ChangeEvent, forwardRef, useEffect, useRef, useState } from "react"
-import { useIntersectionObserver } from "usehooks-ts"
-import { FeatureCarouselNavIndicator } from "./feature-carousel-nav-indicator"
-import { de } from "@faker-js/faker"
-import useDebounce from "@/hooks/use-debounce"
-
-// const DynamicFeature = dynamic(() => import('./feature'), { ssr: false })
-
-const Feature = (props: { number: number, title: string, description: string }) => {
-    const { isIntersecting, ref } = useIntersectionObserver({
-      threshold: 0.5,
-    })
-      
-    return (
-        <div ref={ref} id="Feature" data-key={props.number} className="min-w-full h-fit snap-start grid grid-cols-1 items-center gap-16">
-            <div className="grid lg:grid-cols-2 grid-cols-1 items-center gap-8">
-                <div className="">
-                    <div className="text-6xl">{props.title}</div>
-                </div>
-                <div className="">
-                    {props.description}
-                </div>
-            </div>
-            <div className="grid md:grid-cols-2 grid-cols-1 items-start gap-8">
-                <div className="grid grid-cols-1 items-start gap-8">
-                    <div className="space-y-4">
-                        <div className="font-semibold pl-4 pr-32">Lorem ipsum.</div>
-                        <div className="text-muted-foreground pl-4 pr-32 ">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        </div>
-                    </div>
-                    <div className="space-y-4">
-                        <div className="font-semibold pl-4 pr-32">Lorem ipsum.</div>
-                        <div className="text-muted-foreground pl-4 pr-32">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <Image 
-                        src="/placeholder-browser.svg"
-                        alt="Browser"
-                        width={600}
-                        height={400}
-                    />
-                </div>
-            </div>
-        </div>
-    )
-  }
+import React, { useEffect, useState } from "react"
+import { Feature } from "./feature"
 
 export const FeatureCarousel = () => {
     
-    const [activeFeature, setActiveFeature] = useState(1)
     const [scrollProgress, setScrollProgress] = useState(0)
 
     let featureTrack: HTMLElement | null = null;
     let featureTrackScrollWidth: number = 0;
-    let featureTrackScrollLeft: number = 0;
+    let featureEl: HTMLElement | null = null;
+    let featureElClientWidth: number = 0;
 
     if (typeof document !== 'undefined') {
         featureTrack = document.getElementById("FeatureCarouselTrack");
         featureTrackScrollWidth = featureTrack?.scrollWidth as number;
-        featureTrackScrollLeft = featureTrack?.scrollLeft as number;
+        featureEl = document.getElementById("Feature");
+        featureElClientWidth = featureEl?.clientWidth as number;
     }
 
-    const onTrackScroll = (e: any) => {
-        let progress = ( featureTrackScrollLeft / featureTrackScrollWidth );
+    const onTrackScroll = (e: React.UIEvent<HTMLElement>) => {
+        let progress = Number(( e.currentTarget.scrollLeft / e.currentTarget.scrollWidth ).toPrecision(2)) * 100;
         setScrollProgress(progress);
     }
 
-    featureTrack?.addEventListener('scroll', onTrackScroll);
-
-    useEffect(() => {
-        console.log(scrollProgress, 'scrollProgress')
-        switch (true) {
-            case scrollProgress >= 0 && scrollProgress < 0.25:
-                setActiveFeature(1);
-                break;
-            case scrollProgress >= 0.25 && scrollProgress < 0.5:
-                setActiveFeature(2);
-                break;
-            case scrollProgress >= 0.5 && scrollProgress < 0.75:
-                setActiveFeature(3);
-                break;
-            case scrollProgress >= 0.75 && scrollProgress <= 1:
-                setActiveFeature(4);
-                break;
-        }
-    }, [scrollProgress])
-
     const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
         let id = parseInt(e.currentTarget.id);
-        let n = ( ( id - 1 ) / 4 ) * featureTrackScrollWidth;
+        let n: number = 0;
+        n = ( ( id - 1 ) ) * featureElClientWidth;
+        
         featureTrack?.scrollTo( { left: n , top: 0, behavior: 'smooth' } )
     }
 
     return (
         <div id="FeatureCarouselContainer" className="space-y-8">
-            <div id="FeatureCarouselTrack" className="flex flex-row snap-x snap-mandatory overflow-x-scroll [scrollbar-width:none] overscroll-x-contain py-8 gap-32">
+            <div id="FeatureCarouselTrack" onScroll={onTrackScroll} className="flex flex-row snap-x snap-mandatory overflow-x-scroll [scrollbar-width:none] overscroll-x-contain py-8">
                 <Feature 
                     number={1} 
                     title="Feature 1" 
@@ -125,13 +57,16 @@ export const FeatureCarousel = () => {
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." 
                 />
             </div>
-            <div className="relative grid grid-cols-4 w-full text-center border-collapse">
+            <div className="relative grid grid-cols-4 w-full text-center border-collapse overflow-clip">
+                <div 
+                    className="absolute top-0 w-1/4 h-[1px] bg-primary ease-linear"
+                    style={{ left: `${scrollProgress}%` }} 
+                />
                 <div 
                     id="1" 
                     role="button" 
                     className={cn(
                         "border-t border-dashed border-muted py-8 px-2 text-muted-foreground",
-                        activeFeature === 1 && "border-t border-solid border-t-primary text-foreground"
                     )}
                     onClick={(e) => handleClick(e)}
                 >
@@ -142,7 +77,6 @@ export const FeatureCarousel = () => {
                     role="button" 
                     className={cn(
                         "border-t border-dashed border-muted py-8 px-2 text-muted-foreground",
-                        activeFeature === 2 && "border-t border-solid border-t-primary text-foreground"
                     )}
                     onClick={(e) => handleClick(e)}
                 >
@@ -153,7 +87,6 @@ export const FeatureCarousel = () => {
                     role="button" 
                     className={cn(
                         "border-t border-dashed border-muted py-8 px-2 text-muted-foreground",
-                        activeFeature === 3 && "border-t border-solid border-t-primary text-foreground"
                     )}
                     onClick={(e) => handleClick(e)}
                 >
@@ -164,7 +97,6 @@ export const FeatureCarousel = () => {
                     role="button" 
                     className={cn(
                         "border-t border-dashed border-muted py-8 px-2 text-muted-foreground",
-                        activeFeature === 4 && "border-t border-solid border-t-primary text-foreground"
                     )}
                     onClick={(e) => handleClick(e)}
                 >
--- components/marketing/feature.tsx
+++ components/marketing/feature.tsx
@@ -1,23 +1,51 @@
-const Feature = (({
+import Image from "next/image";
+
+export const Feature = ({
     number,
     title,
-    description,
+    description
 }: {
-    number: number,
-    title: string,
-    description: string,
+    number: number;
+    title: string;
+    description: string;
 }) => {
-    
+      
     return (
-        <div data-key={number} id={`Feature${number}`} className="min-w-full snap-center">
-            <div className="lg:w-1/2">
-                <div className="text-6xl">{title}</div>
+        <div id="Feature" data-key={number} className="min-w-full h-fit snap-start grid grid-cols-1 items-center gap-16 px-16">
+            <div className="grid lg:grid-cols-2 grid-cols-1 items-center gap-8">
+                <div className="">
+                    <div className="text-6xl">{title}</div>
+                </div>
+                <div className="">
+                    {description}
+                </div>
             </div>
-            <div className="lg:w-1/2">
-                {description}
+            <div className="grid md:grid-cols-2 grid-cols-1 items-start gap-8">
+                <div className="grid grid-cols-1 items-start gap-8">
+                    <div className="space-y-4">
+                        <div className="font-semibold pl-4 pr-32">Lorem ipsum.</div>
+                        <div className="text-muted-foreground pl-4 pr-32 ">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                    </div>
+                    <div className="space-y-4">
+                        <div className="font-semibold pl-4 pr-32">Lorem ipsum.</div>
+                        <div className="text-muted-foreground pl-4 pr-32">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <Image 
+                        src="/placeholder-browser.svg"
+                        alt="Browser"
+                        width={600}
+                        height={400}
+                    />
+                </div>
             </div>
         </div>
     )
-})
-
-export default Feature;
+  }
